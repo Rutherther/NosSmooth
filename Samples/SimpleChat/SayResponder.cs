ccdf2299f74571291fdc4fd5df5830b2ce085e6d
František Boháček <fandabohacek@gmail.com>
2021-12-20 20:51:20 +0100

    feat: add simple chat sample

---
+++ Samples/SimpleChat/SayResponder.cs
@@ -0,0 +1,57 @@
+﻿//
+//  SayResponder.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.ServerPackets.Chats;
+using NosCore.Packets.ServerPackets.UI;
+using NosCore.Shared.Enumerations;
+using NosSmooth.Core.Client;
+using NosSmooth.Core.Packets;
+using Remora.Results;
+
+namespace SimpleChat;
+
+/// <summary>
+/// Responds to <see cref="SayPacket"/>.
+/// </summary>
+public class SayResponder : IPacketResponder<SayPacket>, IPacketResponder<MsgPacket>
+{
+    private readonly INostaleClient _client;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SayResponder"/> class.
+    /// </summary>
+    /// <param name="client">The nostale client.</param>
+    public SayResponder(INostaleClient client)
+    {
+        _client = client;
+    }
+
+    /// <inheritdoc />
+    public Task<Result> Respond(SayPacket packet, CancellationToken ct = default)
+    {
+        return _client.ReceivePacketAsync(
+            new SayPacket()
+            {
+                Message = "Hello world from NosSmooth!", VisualType = VisualType.Player, Type = SayColorType.Red, VisualId = 1,
+            },
+            ct);
+    }
+
+    /// <inheritdoc/>
+    public Task<Result> Respond(MsgPacket packet, CancellationToken ct = default)
+    {
+        return _client.ReceivePacketAsync(
+            new SayPacket()
+            {
+                Message = "Hello world from NosSmooth!",
+                VisualType = VisualType.Player,
+                Type = SayColorType.Red,
+                VisualId = 1,
+            },
+            ct);
+    }
+}
---
+++ Samples/SimpleChat/SimpleChat.cs
@@ -0,0 +1,59 @@
+﻿//
+//  SimpleChat.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.ServerPackets.Chats;
+using NosCore.Shared.Enumerations;
+using NosSmooth.Core.Client;
+using NosSmooth.Core.Packets;
+using NosSmooth.LocalClient.Extensions;
+
+namespace SimpleChat;
+
+/// <summary>
+/// The main simple chat class.
+/// </summary>
+public class SimpleChat
+{
+    /// <summary>
+    /// Run the client.
+    /// </summary>
+    /// <returns>The task that may or may not have succeeded.</returns>
+    public async Task RunAsync()
+    {
+        var provider = new ServiceCollection()
+            .AddLocalClient()
+
+            // .AddPacketResponder<SayResponder>()
+            .AddLogging(b =>
+            {
+                b.ClearProviders();
+                b.AddConsole();
+                b.SetMinimumLevel(LogLevel.Debug);
+            })
+            .BuildServiceProvider();
+
+        var dummy1 = provider.GetRequiredService<PacketSerializerProvider>().ServerSerializer;
+        var dummy2 = provider.GetRequiredService<PacketSerializerProvider>().ClientSerializer;
+
+        var logger = provider.GetRequiredService<ILogger<SimpleChat>>();
+        logger.LogInformation("Hello world from SimpleChat!");
+
+        var client = provider.GetRequiredService<INostaleClient>();
+
+        await client.ReceivePacketAsync(new SayPacket()
+        {
+            Message = "Hello world from NosSmooth!",
+            VisualType = VisualType.Player,
+            Type = SayColorType.Red,
+            VisualId = 1,
+        });
+
+        await client.RunAsync();
+    }
+}
