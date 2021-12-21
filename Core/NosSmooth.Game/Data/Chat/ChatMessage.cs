c368b4bda97af9566e595dd0305cd5301a269a87
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:45:43 +0100

    feat: add chat types skeleton

---
+++ Core/NosSmooth.Game/Data/Chat/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿//
+//  ChatMessage.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Entities;
+
+namespace NosSmooth.Game.Data.Chat;
+
+/// <summary>
+/// Message received from user in chat.
+/// </summary>
+/// <param name="CharacterId">The id of the character.</param>
+/// <param name="Player">The player </param>
+/// <param name="Message">The message sent from the friend.</param>
+public record ChatMessage(long CharacterId, Player? Player, string Message);
