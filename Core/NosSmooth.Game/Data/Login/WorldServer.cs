ad70f44f2e906cee23a7f94dca1f6020e8f33000
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:44:34 +0100

    feat: add login types skeleton

---
+++ Core/NosSmooth.Game/Data/Login/WorldServer.cs
@@ -0,0 +1,9 @@
+﻿// 
+// WorldServer.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Login;
+
+public record WorldServer();
