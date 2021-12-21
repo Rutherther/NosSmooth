7ca88f702b6a59e9a5a3629df7b8529e7f47f635
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:45:18 +0100

    feat: add entities types skeleton

---
+++ Core/NosSmooth.Game/Data/Entities/Npc.cs
@@ -0,0 +1,12 @@
+﻿// 
+// Npc.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Entities;
+
+public class Npc
+{
+    
+}
