08b377f45462be02d233ab2ecfc63bf41d5b1eed
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:44:59 +0100

    feat: add position

---
+++ Core/NosSmooth.Game/Data/Position.cs
@@ -0,0 +1,14 @@
+﻿//
+//  Position.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data;
+
+/// <summary>
+/// Represents nostale positition on map.
+/// </summary>
+/// <param name="X">The x coordinate.</param>
+/// <param name="Y">The y coordinate.</param>
+public record Position(int X, int Y);
