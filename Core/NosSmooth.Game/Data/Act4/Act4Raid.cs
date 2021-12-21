c45c1c64c708720e42238666851a16db12524111
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:38:17 +0100

    feat: add act4 status

---
+++ Core/NosSmooth.Game/Data/Act4/Act4Raid.cs
@@ -0,0 +1,33 @@
+﻿//
+//  Act4Raid.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Act4;
+
+/// <summary>
+/// Represents type of raid in act4.
+/// </summary>
+public enum Act4Raid
+{
+    /// <summary>
+    /// Fire element raid with the boss Morcos.
+    /// </summary>
+    Morcos,
+
+    /// <summary>
+    /// Shadow element raid with the boss Hatus.
+    /// </summary>
+    Hatus,
+
+    /// <summary>
+    /// Water element raid with the boss Calvina.
+    /// </summary>
+    Calvina,
+
+    /// <summary>
+    /// Light element raid with the boss Berios.
+    /// </summary>
+    Berios
+}
