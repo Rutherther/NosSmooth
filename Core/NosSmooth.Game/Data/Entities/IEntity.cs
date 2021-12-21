9f5442ff5f33e6d2b255909f73c30ff3028d75b0
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:45:18 +0100

    feat: add entities types skeleton

---
+++ Core/NosSmooth.Game/Data/Entities/IEntity.cs
@@ -0,0 +1,36 @@
+﻿//
+//  IEntity.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Shared.Enumerations;
+using NosSmooth.Game.Data;
+
+namespace NosSmooth.Game.Entities;
+
+/// <summary>
+/// Base type for entities.
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Gets the id of the entity.
+    /// </summary>
+    public long Id { get; }
+
+    /// <summary>
+    /// Gets the name of the entity. May be null if unknown.
+    /// </summary>
+    public string? Name { get; }
+
+    /// <summary>
+    /// Gets the position of the entity.
+    /// </summary>
+    public Position Position { get; }
+
+    /// <summary>
+    /// Gets the type of the entity.
+    /// </summary>
+    public VisualType Type { get; }
+}
