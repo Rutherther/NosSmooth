b71fbf5df5bbd44a6bdf5735c601bb18b2e7566f
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:43:47 +0100

    feat: add nostale game service collection extension

--- Core/NosSmooth.Game/Extensions/ServiceCollectionExtensions.cs
+++ Core/NosSmooth.Game/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,38 @@
+﻿//
+//  ServiceCollectionExtensions.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NosSmooth.Core.Commands;
+using NosSmooth.Core.Extensions;
+using NosSmooth.Game.Events.Handlers;
+
+namespace NosSmooth.Game.Extensions;
+
+/// <summary>
+/// Contains extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds handling of nostale packets, registering <see cref="Game"/> singleton and dispatching of game events.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection.</param>
+    /// <returns>The collection.</returns>
+    public static IServiceCollection AddNostaleGame(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddNostaleCore()
+            .AddMemoryCache()
+            .TryAddSingleton<EventDispatcher>();
+        serviceCollection.TryAddSingleton<Game>();
+
+        // TODO: add events
+        return serviceCollection;
+    }
 
     /// <summary>
     /// Adds the given game event responder.
