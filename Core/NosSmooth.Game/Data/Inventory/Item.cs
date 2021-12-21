1fece65fb638ea7078d0e753a6b982d825008ae5
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:45:07 +0100

    feat: add inventory types skeleton

---
+++ Core/NosSmooth.Game/Data/Inventory/Item.cs
@@ -0,0 +1,12 @@
+﻿// 
+// Item.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Inventory;
+
+public class Item
+{
+    
+}
