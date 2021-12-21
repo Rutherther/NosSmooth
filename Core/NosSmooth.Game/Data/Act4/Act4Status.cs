0f3ec4cb86c060ff8cb0a21c480294ab215b3b09
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:38:17 +0100

    feat: add act4 status

---
+++ Core/NosSmooth.Game/Data/Act4/Act4Status.cs
@@ -0,0 +1,26 @@
+﻿//
+//  Act4Status.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Packets.Enumerations;
+
+namespace NosSmooth.Game.Data.Act4;
+
+/// <summary>
+/// Status of a faction in act4
+/// </summary>
+/// <param name="Percentage">The percentage to Mukraju.</param>
+/// <param name="Mode">The current mode.</param>
+/// <param name="CurrentTime">The current time of the raid.</param>
+/// <param name="TotalTime">The total time the raid will be for.</param>
+/// <param name="Raid">The type of the raid.</param>
+public record Act4FactionStatus
+(
+    short Percentage,
+    Act4Mode Mode,
+    long? CurrentTime,
+    long? TotalTime,
+    Act4Raid Raid
+);
