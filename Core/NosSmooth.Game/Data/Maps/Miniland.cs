0bae3f4cafc990fc2773bfa962d3974922359d7f
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:44:08 +0100

    feat: add maps types skeleton

---
+++ Core/NosSmooth.Game/Data/Maps/Miniland.cs
@@ -0,0 +1,12 @@
+﻿// 
+// Miniland.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Maps;
+
+public class Miniland
+{
+    
+}
