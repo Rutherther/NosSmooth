c4102162ed5c5589ed6c83536a0b8ebb40b32761
František Boháček <fandabohacek@gmail.com>
2021-12-21 22:48:28 +0100

    feat: add raid type skeleton

---
+++ Core/NosSmooth.Game/Data/Raids/Raid.cs
@@ -0,0 +1,12 @@
+﻿// 
+// Raid.cs
+// 
+// Copyright (c) Christofel authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Raids;
+
+public class Raid
+{
+    
+}
