e4b2ea9faa61be1369e4acb98706d7c33bf0a9f2
František Boháček <fandabohacek@gmail.com>
2021-12-23 16:04:56 +0100

    feat: add sample for walk command

---
+++ Samples/WalkCommands/Commands/DetachCommand.cs
@@ -0,0 +1,53 @@
+﻿//
+//  DetachCommand.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.ServerPackets.Chats;
+using NosSmooth.Core.Client;
+using Remora.Results;
+
+namespace WalkCommands.Commands;
+
+/// <summary>
+/// Group for detaching command that detaches the dll.
+/// </summary>
+public class DetachCommand
+{
+    private readonly CancellationTokenSource _dllStop;
+    private readonly INostaleClient _client;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DetachCommand"/> class.
+    /// </summary>
+    /// <param name="dllStop">The cancellation token source to stop the client.</param>
+    /// <param name="client">The nostale client.</param>
+    public DetachCommand(CancellationTokenSource dllStop, INostaleClient client)
+    {
+        _dllStop = dllStop;
+        _client = client;
+    }
+
+    /// <summary>
+    /// Detach the dll.
+    /// </summary>
+    /// <returns>A result that may or may not have succeeded.</returns>
+    public async Task<Result> HandleDetach()
+    {
+        var receiveResult = await _client.ReceivePacketAsync(new SayPacket
+        {
+            Message = "Going to detach!",
+            Type = SayColorType.Green
+        });
+
+        if (!receiveResult.IsSuccess)
+        {
+            return receiveResult;
+        }
+
+        _dllStop.Cancel();
+        return Result.FromSuccess();
+    }
+}
---
+++ Samples/WalkCommands/Startup.cs
@@ -0,0 +1,51 @@
+﻿//
+//  Startup.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NosSmooth.Core.Client;
+using NosSmooth.LocalClient;
+using NosSmooth.LocalClient.Extensions;
+using WalkCommands.Commands;
+
+namespace WalkCommands;
+
+/// <summary>
+/// Startup class of WalkCommands.
+/// </summary>
+public class Startup
+{
+    private IServiceProvider BuildServices()
+    {
+        return new ServiceCollection()
+            .AddLocalClient()
+            .AddScoped<Commands.WalkCommands>()
+            .AddScoped<DetachCommand>()
+            .AddSingleton<CancellationTokenSource>()
+            .Configure<LocalClientOptions>(o => o.AllowIntercept = true)
+            .AddSingleton<IPacketInterceptor, ChatPacketInterceptor>()
+            .AddLogging(b =>
+            {
+                b.ClearProviders();
+                b.AddConsole();
+                b.SetMinimumLevel(LogLevel.Debug);
+            })
+            .BuildServiceProvider();
+    }
+
+    /// <summary>
+    /// Run the MoveToMiniland.
+    /// </summary>
+    /// <returns>A task that may or may not have succeeded.</returns>
+    public async Task RunAsync()
+    {
+        var provider = BuildServices();
+        var mainCancellation = provider.GetRequiredService<CancellationTokenSource>();
+
+        var client = provider.GetRequiredService<INostaleClient>();
+        await client.RunAsync(mainCancellation.Token);
+    }
+}
