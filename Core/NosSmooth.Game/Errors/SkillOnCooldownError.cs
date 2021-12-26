1b8ecea55cb0a459511990af5f46b20f1ea2fa40
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:04:21 +0100

    feat: add events for character skills

---
+++ Core/NosSmooth.Game/Errors/SkillOnCooldownError.cs
@@ -0,0 +1,15 @@
+﻿//
+//  SkillOnCooldownError.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Data.Characters;
+using Remora.Results;
+
+namespace NosSmooth.Game.Errors;
+
+/// <summary>
+/// Acts as an error specifying the skill is on cooldown.
+/// </summary>
+public record SkillOnCooldownError(Skill skill) : ResultError("The skill is on cooldown.");
---
+++ Core/NosSmooth.Game/Events/Characters/SkillReadyEvent.cs
@@ -0,0 +1,14 @@
+﻿//
+//  SkillReadyEvent.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Data.Characters;
+
+namespace NosSmooth.Game.Events.Characters;
+
+/// <summary>
+/// The skill cooldown has been up.
+/// </summary>
+public record SkillReadyEvent(Skill? Skill, long SkillVNum) : IGameEvent;
