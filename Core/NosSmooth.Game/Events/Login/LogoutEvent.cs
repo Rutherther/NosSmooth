333dc88f6e0ed285bbc63f79d81f542592840b77
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:04:26 +0100

    feat: add logout event

---
+++ Core/NosSmooth.Game/Events/Login/LogoutEvent.cs
@@ -0,0 +1,14 @@
+﻿//
+//  LogoutEvent.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Events.Login;
+
+/// <summary>
+/// Represents event that is emitted if the user is logged out / changing the server etc.
+/// </summary>
+public class LogoutEvent : IGameEvent
+{
+}
