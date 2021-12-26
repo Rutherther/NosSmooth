618132b12648e2fc0f0b5d1772111ed81edc9941
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:04:21 +0100

    feat: add events for character skills

---
+++ Core/NosSmooth.Game/Events/Characters/ReceivedCharacterDataEvent.cs
@@ -0,0 +1,20 @@
+﻿//
+//  ReceivedCharacterDataEvent.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Data.Characters;
+
+namespace NosSmooth.Game.Events.Characters;
+
+/// <summary>
+/// Represents received new updated character data.
+/// </summary>
+/// <param name="OldCharacter">The old data.</param>
+/// <param name="Character">The newly received data.</param>
+public record ReceivedCharacterDataEvent
+(
+    Character? OldCharacter,
+    Character Character
+) : IGameEvent;
---
+++ Core/NosSmooth.Game/Events/Characters/SkillsReceivedEvent.cs
@@ -0,0 +1,11 @@
+﻿//
+//  SkillsReceivedEvent.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Data.Characters;
+
+namespace NosSmooth.Game.Events.Characters;
+
+public record SkillsReceivedEvent(Skills Skills) : IGameEvent;
