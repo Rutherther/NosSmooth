d5d868ef2fc037b8ec5e7993743d14b0c843a8ad
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:02:45 +0100

    chore: move data classes to correct namespaces

---
+++ Core/NosSmooth.Game/Data/Info/Position.cs
@@ -0,0 +1,23 @@
+﻿//
+//  Position.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Info;
+
+/// <summary>
+/// Represents nostale position on map.
+/// </summary>
+public record Position
+{
+    /// <summary>
+    /// Gets the x coordinate.
+    /// </summary>
+    public long X { get; internal set; }
+
+    /// <summary>
+    /// Gets the y coordinate.
+    /// </summary>
+    public long Y { get; internal set; }
+}
---
+++ Core/NosSmooth.Game/Data/Social/Family.cs
@@ -0,0 +1,15 @@
+﻿//
+//  Family.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Social;
+
+/// <summary>
+/// Represents nostale family entity.
+/// </summary>
+/// <param name="Id">The id of the family.</param>
+/// <param name="Name">The name of the family.</param>
+/// <param name="Level">The level of the entity.</param>
+public record Family(string? Id, string? Name, byte Level);
