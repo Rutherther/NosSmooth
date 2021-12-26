b934bd6736986413524b4a566f87477176b523dd
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:07:14 +0100

    feat: add helper data records

---
+++ Core/NosSmooth.Game/Data/Characters/Skills.cs
@@ -0,0 +1,20 @@
+﻿//
+//  Skills.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Characters;
+
+/// <summary>
+/// Holds skill of a Character.
+/// </summary>
+/// <param name="PrimarySkill">The VNum of the primary skill. This skill is used with the primary weapon. (Could be different for sp cards.)</param>
+/// <param name="SecondarySkill">The VNum of the secondary skill. This skill is used with the secondary weapon. (Could be different for sp cards)</param>
+/// <param name="OtherSkills">The VNums of other skills.</param>
+public record Skills
+(
+    Skill PrimarySkill,
+    Skill SecondarySkill,
+    IReadOnlyList<Skill> OtherSkills
+);
---
+++ Core/NosSmooth.Game/Data/Info/Health.cs
@@ -0,0 +1,17 @@
+﻿//
+//  Health.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Info;
+
+/// <summary>
+/// Represents the health or mana of an entity.
+/// </summary>
+/// <param name="Amount">The current amount of health.</param>
+/// <param name="Maximum">The maximum amount of health.</param>
+public record Health(long Amount, long Maximum)
+{
+    private decimal Percentage => (decimal)Amount / Maximum;
+}
