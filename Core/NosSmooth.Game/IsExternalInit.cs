62ca8648dc68f081e0c1ee2efe2e70b7981d4746
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:08:04 +0100

    chore: add IsExternalInit to fix records for netstandard 2.0

---
+++ Core/NosSmooth.Game/IsExternalInit.cs
@@ -0,0 +1,16 @@
+﻿//
+//  IsExternalInit.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Dummy.
+    /// </summary>
+    public class IsExternalInit
+    {
+    }
+}
