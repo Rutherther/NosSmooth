0cc7d3af01a1e2a47d1f7da5a0159298b77375b3
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:04:35 +0100

    feat: add moved event

---
+++ Core/NosSmooth.Game/Events/Entities/MovedEvent.cs
@@ -0,0 +1,18 @@
+﻿//
+//  MovedEvent.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Game.Data.Entities;
+using NosSmooth.Game.Data.Info;
+
+namespace NosSmooth.Game.Events.Entities;
+
+public record MovedEvent
+(
+    IEntity Entity,
+    long EntityId,
+    Position? OldPosition,
+    Position NewPosition
+) : IGameEvent;
