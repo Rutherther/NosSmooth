a94a95b0efff57ccc7a8c70a8c9f4cdb0a1beccb
František Boháček <fandabohacek@gmail.com>
2021-12-26 23:07:14 +0100

    feat: add helper data records

---
+++ Core/NosSmooth.Game/Data/Info/Morph.cs
@@ -0,0 +1,28 @@
+﻿//
+//  Morph.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Game.Data.Info;
+
+/// <summary>
+/// Represents players morph.
+/// </summary>
+/// <remarks>
+/// Morphs are used mainly for special cards.
+/// The VNum will contain the vnum of the special card.
+/// </remarks>
+/// <param name="VNum">The vnum of the morph.</param>
+/// <param name="Upgrade">The upgrade to show wings.</param>
+/// <param name="Design">The design of the wings.</param>
+/// <param name="Bonus">Unknown.</param>
+/// <param name="Skin">The skin of the wings.</param>
+public record Morph
+(
+    long VNum,
+    byte Upgrade,
+    short? Design = default,
+    byte? Bonus = default,
+    short? Skin = default
+);
