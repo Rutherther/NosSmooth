9b14d09b35b14705624898307b086957af128748
František Boháček <fandabohacek@gmail.com>
2021-12-29 17:29:54 +0100

    chore: rename parameter serializer error

--- Core/NosSmooth.Packets/Errors/PacketParameterSerializerError.cs
+++ Core/NosSmooth.Packets/Errors/PacketParameterSerializerError.cs
@@ -1,5 +1,5 @@
 //
-//  PacketParameterDeserializeError.cs
+//  PacketParameterSerializerError.cs
 //
 //  Copyright (c) František Boháček. All rights reserved.
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
@@ -16,5 +16,5 @@ namespace NosSmooth.Packets.Errors;
 /// <param name="Converter">The converter that could not deserialize the parameter.</param>
 /// <param name="PropertyName">The name of the property.</param>
 /// <param name="Result">The underlying result.</param>
-public record PacketParameterDeserializeError(ITypeConverter Converter, string PropertyName, IResult Result)
+public record PacketParameterSerializerError(ITypeConverter Converter, string PropertyName, IResult Result)
     : ResultError($"There was an error deserializing property {PropertyName} in converter {Converter.GetType().FullName}");
