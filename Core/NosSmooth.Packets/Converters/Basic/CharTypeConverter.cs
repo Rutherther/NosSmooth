11ff4313111e807f61fff6db55f578272aaac424
František Boháček <fandabohacek@gmail.com>
2021-12-29 15:15:24 +0100

    feat: add char and string type converters

---
+++ Core/NosSmooth.Packets/Converters/Basic/CharTypeConverter.cs
@@ -0,0 +1,27 @@
+//
+//  CharTypeConverter.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Packets.Errors;
+using Remora.Results;
+
+namespace NosSmooth.Packets.Converters.Basic;
+
+/// <summary>
+/// Converter of <see cref="char"/>.
+/// </summary>
+public class CharTypeConverter : BasicTypeConverter<char>
+{
+    /// <inheritdoc />
+    protected override Result<char> Deserialize(string value)
+    {
+        if (value.Length != 1)
+        {
+            return new CouldNotConvertError(this, value, "The token is not one character long.");
+        }
+
+        return value[0];
+    }
+}
