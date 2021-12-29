ddc307b5e215134ba22ec9535ecc49ae6305c46a
František Boháček <fandabohacek@gmail.com>
2021-12-30 00:29:06 +0100

    feat: add bool type converter

---
+++ Core/NosSmooth.Packets/Converters/Basic/BoolTypeConverter.cs
@@ -0,0 +1,40 @@
+//
+//  BoolTypeConverter.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Packets.Errors;
+using Remora.Results;
+
+namespace NosSmooth.Packets.Converters.Basic;
+
+/// <summary>
+/// Converter of <see cref="bool"/>.
+/// </summary>
+public class BoolTypeConverter : BaseTypeConverter<bool>
+{
+    /// <inheritdoc />
+    public override Result Serialize(bool obj, PacketStringBuilder builder)
+    {
+        builder.Append(obj ? "1" : "0");
+        return Result.FromSuccess();
+    }
+
+    /// <inheritdoc />
+    public override Result<bool> Deserialize(PacketStringEnumerator stringEnumerator)
+    {
+        var nextTokenResult = stringEnumerator.GetNextToken();
+        if (!nextTokenResult.IsSuccess)
+        {
+            return Result<bool>.FromError(nextTokenResult);
+        }
+
+        if (nextTokenResult.Entity.Token == "-")
+        {
+            return Result<bool>.FromSuccess(default);
+        }
+
+        return nextTokenResult.Entity.Token == "1" ? true : false;
+    }
+}
