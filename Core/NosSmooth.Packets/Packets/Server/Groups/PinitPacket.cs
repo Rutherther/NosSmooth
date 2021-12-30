d610c7b531a24406a55ee6beb4eae2ecd1198596
František Boháček <fandabohacek@gmail.com>
2021-12-30 10:58:10 +0100

    feat: add pinit packet

---
+++ Core/NosSmooth.Packets/Packets/Server/Groups/PinitPacket.cs
@@ -0,0 +1,27 @@
+//
+//  PinitPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using NosSmooth.Packets.Attributes;
+
+namespace NosSmooth.Packets.Packets.Server.Groups;
+
+/// <summary>
+/// Sent for initialization of pets and groups.
+/// </summary>
+/// <remarks>
+/// Contains pet and group information.
+/// </remarks>
+/// <param name="GroupSize"></param>
+/// <param name="PinitSubPackets"></param>
+[GenerateSerializer]
+[PacketHeader("pinit", PacketSource.Server)]
+public record PinitPacket
+(
+    [PacketIndex(0)] int GroupSize,
+    [PacketListIndex(1, ListSeparator = ' ', InnerSeparator = '|')]
+    IReadOnlyList<PinitSubPacket> PinitSubPackets
+) : IPacket;
---
+++ Core/NosSmooth.Packets/Packets/Server/Groups/PinitSubPacket.cs
@@ -0,0 +1,48 @@
+//
+//  PinitSubPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosCore.Shared.Enumerations;
+using NosSmooth.Packets.Attributes;
+
+namespace NosSmooth.Packets.Packets.Server.Groups;
+
+/// <summary>
+/// Sub packet of <see cref="PinitSubPacket"/> containing information
+/// about one of the group members.
+/// </summary>
+/// <param name="EntityType">The type of the entity.</param>
+/// <param name="EntityId">The id of the entity.</param>
+/// <param name="GroupPosition">The position in the group.</param>
+/// <param name="Level">The level of the entity.</param>
+/// <param name="Name">The name of the entity.</param>
+/// <param name="Unknown">Unknown.</param>
+/// <param name="VNum">The VNum of the pet for pets.</param>
+/// <param name="Race">The race of the entity.</param>
+/// <param name="Morph">The morph of the entity.</param>
+/// <param name="HeroLevel">The hero level of the entity.</param>
+[GenerateSerializer]
+[PacketHeader(null, PacketSource.Server)]
+public record PinitSubPacket
+(
+    [PacketIndex(0)]
+    VisualType EntityType,
+    [PacketIndex(1)]
+    long EntityId,
+    [PacketIndex(2)]
+    int GroupPosition,
+    [PacketIndex(3)]
+    byte Level,
+    [PacketIndex(4)]
+    string? Name,
+    [PacketIndex(5)]
+    int Unknown,
+    [PacketIndex(6)]
+    long VNum,
+    [PacketIndex(7)]
+    short Race,
+    [PacketIndex(8)]
+    short Morph
+) : IPacket;
