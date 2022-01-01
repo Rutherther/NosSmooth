f70a98fc68d9ff34e8d4213ecb92c87bfd1e8fa6
František Boháček <fandabohacek@gmail.com>
2022-01-01 20:57:40 +0100

    feat: add client SayPacket

---
+++ Core/NosSmooth.Packets/Packets/Client/Chat/SayPacket.cs
@@ -0,0 +1,17 @@
+﻿//
+//  SayPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Packets.Attributes;
+
+namespace NosSmooth.Packets.Packets.Client.Chat;
+
+[PacketHeader("say", PacketSource.Client)]
+[GenerateSerializer(true)]
+public record SayPacket
+(
+    [PacketGreedyIndex(0)]
+    string Message
+) : IPacket;
