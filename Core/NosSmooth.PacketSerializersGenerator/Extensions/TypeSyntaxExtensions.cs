de7be8043d24c53acb4754d4d34c07be7c304376
František Boháček <fandabohacek@gmail.com>
2022-01-02 14:31:42 +0100

    feat: add extensions for checking type nullability

---
+++ Core/NosSmooth.PacketSerializersGenerator/Extensions/TypeSyntaxExtensions.cs
@@ -0,0 +1,25 @@
+//
+//  TypeSyntaxExtensions.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NosSmooth.PacketSerializersGenerator.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="TypeSyntax"/>.
+/// </summary>
+public static class TypeSyntaxExtensions
+{
+    /// <summary>
+    /// Gets whether the type syntax is nullable.
+    /// </summary>
+    /// <param name="typeSyntax">The type syntax.</param>
+    /// <returns>Whether the type syntax is nullable.</returns>
+    public static bool IsNullable(this TypeSyntax typeSyntax)
+    {
+        return typeSyntax is NullableTypeSyntax;
+    }
+}
