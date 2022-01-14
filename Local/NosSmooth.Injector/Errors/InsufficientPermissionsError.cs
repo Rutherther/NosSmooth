5c6f9bb964d1438e6163a567a49ee21df6272951
František Boháček <fandabohacek@gmail.com>
2022-01-14 20:08:18 +0100

    feat: add .net 5+ injector

---
+++ Local/NosSmooth.Injector/Errors/InsufficientPermissionsError.cs
@@ -0,0 +1,20 @@
+﻿//
+//  InsufficientPermissionsError.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Remora.Results;
+
+namespace NosSmooth.Injector.Errors;
+
+/// <summary>
+/// The current user has insufficient permissions to inject into the given process.
+/// </summary>
+/// <param name="ProcessId">The id of the process.</param>
+/// <param name="ProcessName">The name of the process.</param>
+public record InsufficientPermissionsError(long ProcessId, string ProcessName)
+    : ResultError
+    (
+        $"Insufficient permissions to open process {ProcessId} ({ProcessName}). Try running the injector as administrator."
+    );
---
+++ Local/NosSmooth.Injector/NosInjectorOptions.cs
@@ -0,0 +1,32 @@
+﻿//
+//  NosInjectorOptions.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace NosSmooth.Injector
+{
+    /// <summary>
+    /// Options for NosInjector.
+    /// </summary>
+    public class NosInjectorOptions : IOptions<NosInjectorOptions>
+    {
+        /// <summary>
+        /// Gets or sets the path to the nos smooth inject dll.
+        /// </summary>
+        /// <remarks>
+        /// If not absolute path, then relative path from the current executing process is assumed.
+        /// </remarks>
+        public string NosSmoothInjectPath { get; set; } = "NosSmooth.Inject.dll";
+
+        /// <inheritdoc/>
+        public NosInjectorOptions Value => this;
+    }
+}
