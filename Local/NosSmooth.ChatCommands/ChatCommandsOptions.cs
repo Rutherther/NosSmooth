e1ab9a1ac7bf75a062ffab6e946fd38bec557c9c
František Boháček <fandabohacek@gmail.com>
2022-01-20 19:25:55 +0100

    feat(chatcommands): add support for chat commands using Remora.Commands

---
+++ Local/NosSmooth.ChatCommands/ChatCommandsOptions.cs
@@ -0,0 +1,18 @@
+﻿//
+//  ChatCommandsOptions.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.ChatCommands;
+
+/// <summary>
+/// Options for <see cref="ChatCommandInterceptor"/>.
+/// </summary>
+public class ChatCommandsOptions
+{
+    /// <summary>
+    /// Gets or sets the command prefix.
+    /// </summary>
+    public string Prefix { get; set; } = "#";
+}
