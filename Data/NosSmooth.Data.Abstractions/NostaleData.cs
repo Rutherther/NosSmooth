d4c76de4dffbc3d942948ed7a7403028fae8bd23
Rutherther <fandabohacek@gmail.com>
2022-02-03 18:47:22 +0100

    docs(data): add documentation for NostaleData

--- Data/NosSmooth.Data.Abstractions/NostaleData.cs
+++ Data/NosSmooth.Data.Abstractions/NostaleData.cs
@@ -9,6 +9,14 @@ using NosSmooth.Data.Abstractions.Language;
 
 namespace NosSmooth.Data.Abstractions;
 
+/// <summary>
+/// The parsed NosTale data.
+/// </summary>
+/// <param name="Translations">The language translations.</param>
+/// <param name="Items">The items information.</param>
+/// <param name="Monsters">The monsters information.</param>
+/// <param name="Skills">The skills information.</param>
+/// <param name="Maps">The maps information.</param>
 public record NostaleData
 (
     IReadOnlyDictionary<Language.Language, IReadOnlyDictionary<TranslationRoot, IReadOnlyDictionary<string, string>>> Translations,
