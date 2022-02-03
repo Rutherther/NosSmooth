c2cd17a4f24f78b49e960117d305b1714334161d
Rutherther <fandabohacek@gmail.com>
2022-02-03 18:47:45 +0100

    docs(data): add documentation for ReadFile

--- Data/NosSmooth.Data.NOSFiles/Files/ReadFile.cs
+++ Data/NosSmooth.Data.NOSFiles/Files/ReadFile.cs
@@ -8,6 +8,12 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace NosSmooth.Data.NOSFiles.Files;
 
+/// <summary>
+/// A file that has been already processed by a reader.
+/// </summary>
+/// <param name="Path">A path to the file.</param>
+/// <param name="Content">The content read.</param>
+/// <typeparam name="TContent">The type of the content read.</typeparam>
 [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Upper case is standard.")]
 public record struct ReadFile<TContent>
 (
