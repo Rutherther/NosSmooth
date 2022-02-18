c7de76b4a58cfee38c8eee2f6bcc0efc08af12c2
Rutherther <fandabohacek@gmail.com>
2022-02-18 23:33:40 +0100

    feat(combat): add combat manager

---
+++ Extensions/NosSmooth.Extensions.Combat/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿//
+//  ServiceCollectionExtensions.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using NosSmooth.Core.Extensions;
+using NosSmooth.Extensions.Combat.Responders;
+
+namespace NosSmooth.Extensions.Combat.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds a NosTale combat extension. <see cref="CombatManager"/>.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection.</param>
+    /// <returns>The collection.</returns>
+    public static IServiceCollection AddNostaleCombat(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddPacketResponder<CancelResponder>()
+            .AddPacketResponder<SuResponder>()
+            .AddSingleton<CombatManager>();
+    }
+}
