03e7d49a76478bad3f1d367847dc9e0bb953a454
František Boháček <fandabohacek@gmail.com>
2022-02-19 09:16:30 +0100

    docs(combat): add missing documentation

--- Extensions/NosSmooth.Extensions.Combat/Techniques/ICombatTechnique.cs
+++ Extensions/NosSmooth.Extensions.Combat/Techniques/ICombatTechnique.cs
@@ -18,9 +18,32 @@ namespace NosSmooth.Extensions.Combat.Techniques;
 /// </remarks>
 public interface ICombatTechnique
 {
+    /// <summary>
+    /// Should check whether the technique should process more steps or quit the combat.
+    /// </summary>
+    /// <param name="state">The combat state.</param>
+    /// <returns>Whether to continue with steps.</returns>
     public bool ShouldContinue(ICombatState state);
 
+    /// <summary>
+    /// Handle one step that should enqueue an operation.
+    /// </summary>
+    /// <remarks>
+    /// If error is returned, the combat will be cancelled.
+    /// </remarks>
+    /// <param name="state">The combat state.</param>
+    /// <returns>A result that may or may not succeed.</returns>
     public Result HandleCombatStep(ICombatState state);
 
+    /// <summary>
+    /// Handles an error from <see cref="ICombatOperation.UseAsync"/>.
+    /// </summary>
+    /// <remarks>
+    /// If an error is returned, the combat will be cancelled.
+    /// </remarks>
+    /// <param name="state">The combat state.</param>
+    /// <param name="operation">The combat operation that returned an error.</param>
+    /// <param name="result">The errorful result.</param>
+    /// <returns>A result that may or may not succeed.</returns>
     public Result HandleError(ICombatState state, ICombatOperation operation, Result result);
 }
