db9f6cefbf99e51cb299223acd701aac13fa2121
František Boháček <fandabohacek@gmail.com>
2023-01-03 20:34:34 +0100

    chore: make Core internals visible to tests

---
+++ Core/NosSmooth.Core/Attributes.cs
@@ -0,0 +1,9 @@
+//
+//  Attributes.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NosSmooth.Core.Tests")]
