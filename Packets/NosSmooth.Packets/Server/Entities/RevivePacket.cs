b54039fcfc7c9184908f5601ac07361be94e234a
Rutherther <fandabohacek@gmail.com>
2023-01-06 23:34:53 +0100

    feat(game): add revive handling

---
+++ Packets/NosSmooth.Packets/Server/Entities/RevivePacket.cs
@@ -0,0 +1,22 @@
+﻿//
+//  RevivePacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.Packets.Enums.Entities;
+using NosSmooth.PacketSerializer.Abstractions.Attributes;
+
+namespace NosSmooth.Packets.Server.Entities;
+
+[PacketHeader("revive", PacketSource.Server)]
+[GenerateSerializer(true)]
+public record RevivePacket
+(
+    [PacketIndex(0)]
+    EntityType EntityType,
+    [PacketIndex(1)]
+    long EntityId,
+    [PacketIndex(2)]
+    short TimeSpaceLives
+) : IPacket;
