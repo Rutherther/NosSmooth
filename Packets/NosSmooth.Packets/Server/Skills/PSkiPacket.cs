3c40a905f7e888a510428fa226313922eb7b7029
Rutherther <fandabohacek@gmail.com>
2023-01-06 22:56:20 +0100

    feat(packets): add pet and partner skill packets

---
+++ Packets/NosSmooth.Packets/Server/Skills/PSkiPacket.cs
@@ -0,0 +1,20 @@
+﻿//
+//  PSkiPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.PacketSerializer.Abstractions.Attributes;
+
+namespace NosSmooth.Packets.Server.Skills;
+
+/// <summary>
+/// Partner skills.
+/// </summary>
+[PacketHeader("pski", PacketSource.Server)]
+[GenerateSerializer(true)]
+public record PSkiPacket
+(
+    [PacketListIndex(0, ListSeparator = ' ')]
+    IReadOnlyList<int?> SkillVNums
+) : IPacket;
---
+++ Packets/NosSmooth.Packets/Server/Skills/PetskiPacket.cs
@@ -0,0 +1,21 @@
+﻿//
+//  PetskiPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.PacketSerializer.Abstractions.Attributes;
+
+namespace NosSmooth.Packets.Server.Skills;
+
+/// <summary>
+/// Information about pet skills.
+/// </summary>
+/// <param name="SkillVNum">The vnum of the pet skill.</param>
+[PacketHeader("petski", PacketSource.Server)]
+[GenerateSerializer(true)]
+public record PetskiPacket
+(
+    [PacketIndex(0)]
+    int? SkillVNum
+) : IPacket;
