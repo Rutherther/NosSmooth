42f30664622da2fe7673fee2c61b432b14aa51e8
Rutherther <fandabohacek@gmail.com>
2023-01-06 22:56:06 +0100

    feat(packets): add pclear packet

---
+++ Packets/NosSmooth.Packets/Server/Groups/PClearPacket.cs
@@ -0,0 +1,16 @@
+﻿//
+//  PClearPacket.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NosSmooth.PacketSerializer.Abstractions.Attributes;
+
+namespace NosSmooth.Packets.Server.Groups;
+
+/// <summary>
+/// Clear the characters's group.
+/// </summary>
+[PacketHeader("p_clear", PacketSource.Server)]
+[GenerateSerializer(true)]
+public record PClearPacket() : IPacket;
