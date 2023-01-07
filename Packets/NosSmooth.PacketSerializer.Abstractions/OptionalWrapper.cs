e8b40f7a9754f317e10346f6ec0853f2dde2cc21
Rutherther <fandabohacek@gmail.com>
2023-01-07 22:09:02 +0100

    feat(packets): add optional wrapper for wrapping optional values inside of a list

---
+++ Packets/NosSmooth.PacketSerializer.Abstractions/OptionalWrapper.cs
@@ -0,0 +1,41 @@
+﻿//
+//  OptionalWrapper.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace NosSmooth.PacketSerializer.Abstractions;
+
+/// <summary>
+/// Wraps a compound value that may not be present
+/// and there will be nothing instead in the packet.
+/// The converter of underlying type will be called
+/// if and only if the value is not null.
+/// </summary>
+/// <param name="Value">The value.</param>
+/// <typeparam name="T">The underlying type.</typeparam>
+[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Fix this, this should not happen.")]
+public record struct OptionalWrapper<T>(bool Present, T? Value)
+{
+    /// <summary>
+    /// Unwrap the underlying value.
+    /// </summary>
+    /// <param name="wrapper">The wrapper to unwrap.</param>
+    /// <returns>The unwrapped value.</returns>
+    public static implicit operator T?(OptionalWrapper<T> wrapper)
+    {
+        return wrapper.Value;
+    }
+
+    /// <summary>
+    /// wrap the value in optional wrapper.
+    /// </summary>
+    /// <param name="value">The value to wrap.</param>
+    /// <returns>The wrapped value.</returns>
+    public static implicit operator OptionalWrapper<T>(T? value)
+    {
+        return new OptionalWrapper<T>(true, value);
+    }
+}
---
+++ Packets/NosSmooth.PacketSerializer/Converters/Common/OptionalWrapperConverter.cs
@@ -0,0 +1,82 @@
+﻿//
+//  OptionalWrapperConverter.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using NosSmooth.PacketSerializer.Abstractions;
+using Remora.Results;
+
+namespace NosSmooth.PacketSerializer.Converters.Common;
+
+/// <summary>
+/// Converter of <see cref="OptionalWrapper{T}"/>.
+/// </summary>
+/// <typeparam name="T">The underlying type.</typeparam>
+public class OptionalWrapperConverter<T> : BaseStringConverter<OptionalWrapper<T>>
+{
+    private readonly IStringConverterRepository _converterRepository;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OptionalWrapperConverter{T}"/> class.
+    /// </summary>
+    /// <param name="converterRepository">The converter repository.</param>
+    public OptionalWrapperConverter(IStringConverterRepository converterRepository)
+    {
+        _converterRepository = converterRepository;
+    }
+
+    /// <inheritdoc />
+    public override Result Serialize(OptionalWrapper<T> obj, PacketStringBuilder builder)
+    {
+        if (obj.Value is null)
+        {
+            return Result.FromSuccess();
+        }
+
+        var converterResult = _converterRepository.GetTypeConverter<T>();
+        if (!converterResult.IsDefined(out var converter))
+        {
+            return Result.FromError(converterResult);
+        }
+
+        return converter.Serialize(obj.Value, builder);
+
+    }
+
+    /// <inheritdoc />
+    public override Result<OptionalWrapper<T>> Deserialize(ref PacketStringEnumerator stringEnumerator, DeserializeOptions options)
+    {
+        if (stringEnumerator.IsOnLastToken() ?? false)
+        {
+            return Result<OptionalWrapper<T>>.FromSuccess(new OptionalWrapper<T>(false, default));
+        }
+
+        var tokenResult = stringEnumerator.GetNextToken(out var token, false);
+        if (!tokenResult.IsSuccess)
+        {
+            return Result<OptionalWrapper<T>>.FromError(tokenResult);
+        }
+
+        if (token.Token.Length == 0)
+        {
+            stringEnumerator.GetNextToken(out _); // seek
+            return Result<OptionalWrapper<T>>.FromSuccess(new OptionalWrapper<T>(false, default));
+        }
+
+        var converterResult = _converterRepository.GetTypeConverter<T>();
+        if (!converterResult.IsDefined(out var converter))
+        {
+            return Result<OptionalWrapper<T>>.FromError(converterResult);
+        }
+
+        var deserializationResult = converter.Deserialize(ref stringEnumerator, new DeserializeOptions(true));
+        if (!deserializationResult.IsDefined(out var deserialization))
+        {
+            return Result<OptionalWrapper<T>>.FromError(deserializationResult);
+        }
+
+        return new OptionalWrapper<T>(true, deserialization);
+    }
+}
