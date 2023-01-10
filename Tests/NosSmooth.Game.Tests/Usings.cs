0f3e4eab60206840ebea6a6a25df2ef731f3d557
Rutherther <fandabohacek@gmail.com>
2023-01-10 23:14:37 +0100

    chore: use Shouldly

--- Tests/NosSmooth.Game.Tests/Usings.cs
+++ Tests/NosSmooth.Game.Tests/Usings.cs
@@ -4,4 +4,5 @@
 //  Copyright (c) František Boháček. All rights reserved.
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+global using Shouldly;
 global using Xunit;
