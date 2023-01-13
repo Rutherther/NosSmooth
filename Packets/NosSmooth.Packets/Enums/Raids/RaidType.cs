e4b2b3cbab68426c8f62c8c7387499a6810f74f2
Rutherther <fandabohacek@gmail.com>
2023-01-13 20:02:37 +0100

    feat(packets): add raid types

---
+++ Packets/NosSmooth.Packets/Enums/Raids/RaidType.cs
@@ -0,0 +1,50 @@
+﻿//
+//  RaidType.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace NosSmooth.Packets.Enums.Raids;
+
+/// <summary>
+/// A type of a raid.
+/// </summary>
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "Self explanatory.")]
+public enum RaidType
+{
+    Cuby = 0,
+    Ginseng = 1,
+    Castra = 2,
+    GiantBlackSpider = 3,
+    Slade = 4,
+    ChickenKing = 5,
+    Namaju = 6,
+    Grasslin = 7,
+    Snowman = 8,
+    RobberGang = 9,
+    JackOLantern = 10,
+    ChickenQueen = 11,
+    Pirate = 12,
+    Kertos = 13,
+    Valakus = 14,
+    Grenigas = 15,
+    LordDraco = 16,
+    Glacerus = 17,
+    Foxy = 18,
+    Maru = 19,
+    Laurena = 20,
+    HongbiCheongbi = 21,
+    LolaLopears = 22,
+    Zenas = 23,
+    Erenia = 24,
+    Fernon = 25,
+    Fafnir = 26,
+    Yertirand = 27,
+    MadProffesor = 28,
+    MadMarchHare = 29,
+    Kirollas = 30,
+    Carno = 31,
+    Belial = 32
+}
--- Packets/NosSmooth.Packets/Server/Raids/RdlstPacket.cs
+++ Packets/NosSmooth.Packets/Server/Raids/RdlstPacket.cs
@@ -4,6 +4,7 @@
 //  Copyright (c) František Boháček. All rights reserved.
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using NosSmooth.Packets.Enums.Raids;
 using NosSmooth.PacketSerializer.Abstractions.Attributes;
 
 namespace NosSmooth.Packets.Server.Raids;
@@ -25,7 +26,7 @@ public record RdlstPacket
     [PacketIndex(1)]
     byte MaximumLevel,
     [PacketIndex(2)]
-    byte RaidType,
+    RaidType RaidType,
     [PacketIndex(3)]
     IReadOnlyList<RdlstSubPacket> Players
 ) : IPacket;
