7a30c95d162ec3a3ce1691f7b72b20e515a7c785
Rutherther <fandabohacek@gmail.com>
2023-01-19 18:26:28 +0100

    tests(core): add tests for contracts

---
+++ Tests/NosSmooth.Core.Tests/Contracts/ContractMultipleStates.cs
@@ -0,0 +1,33 @@
+﻿//
+//  ContractMultipleStates.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NosSmooth.Core.Tests.Contracts;
+
+/// <summary>
+/// Extends DefaultStates to have more states to test.
+/// </summary>
+public enum ContractMultipleStates
+{
+    /// <summary>
+    /// Initial state.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Contract executed, request issued.
+    /// </summary>
+    Requested,
+
+    /// <summary>
+    /// A response was obtained.
+    /// </summary>
+    ResponseObtained,
+
+    /// <summary>
+    /// Something else happening after obtaining first response.
+    /// </summary>
+    AfterResponseObtained,
+}
