302128623bf84abd788dcebdf74135261115a220
Rutherther <fandabohacek@gmail.com>
2023-02-11 18:13:39 +0100

    docs(pcap): add documentation to pcap records

--- Pcap/NosSmooth.Pcap/ConnectionData.cs
+++ Pcap/NosSmooth.Pcap/ConnectionData.cs
@@ -8,6 +8,15 @@ using System.Net;
 
 namespace NosSmooth.Pcap;
 
+/// <summary>
+/// Data from a tcp connection containing first few sniffed packets.
+/// </summary>
+/// <param name="SourceAddress">The packets source address.</param>
+/// <param name="SourcePort">The packets source port.</param>
+/// <param name="DestinationAddress">The packets destination address.</param>
+/// <param name="DestinationPort">The packets destination port.</param>
+/// <param name="SniffedData">The sniffed data.</param>
+/// <param name="FirstObservedAt">The time first data were observed at.</param>
 public record ConnectionData
 (
     IPAddress SourceAddress,
