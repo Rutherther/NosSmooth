5c10978909817964303732e9031b6cefa61af137
Rutherther <fandabohacek@gmail.com>
2023-02-11 17:33:46 +0100

    feat(pcap): add pcap client support

---
+++ Pcap/NosSmooth.Pcap/ConnectionData.cs
@@ -0,0 +1,19 @@
+﻿//
+//  ConnectionData.cs
+//
+//  Copyright (c) František Boháček. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net;
+
+namespace NosSmooth.Pcap;
+
+public record ConnectionData
+(
+    IPAddress SourceAddress,
+    int SourcePort,
+    IPAddress DestinationAddress,
+    int DestinationPort,
+    List<byte[]> SniffedData,
+    DateTimeOffset FirstObservedAt
+);
