59aa7ebebe54c78d423a18b43b2f826da65b7b80
Rutherther <fandabohacek@gmail.com>
2023-02-13 17:43:36 +0100

    feat(core): make PacketEventArgs structs

--- Core/NosSmooth.Core/Packets/PacketEventArgs.cs
+++ Core/NosSmooth.Core/Packets/PacketEventArgs.cs
@@ -4,6 +4,8 @@
 //  Copyright (c) František Boháček. All rights reserved.
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Diagnostics.CodeAnalysis;
 using NosSmooth.PacketSerializer.Abstractions.Attributes;
 
 namespace NosSmooth.Core.Packets;
@@ -13,7 +15,8 @@ namespace NosSmooth.Core.Packets;
 /// </summary>
 /// <param name="Source">The source of the packet.</param>
 /// <param name="PacketString">The packet string.</param>
-public record PacketEventArgs(PacketSource Source, string PacketString);
+[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Fix this.")]
+public record struct PacketEventArgs(PacketSource Source, string PacketString);
 
 /// <summary>
 /// Arguments for <see cref="IPacketResponder{TPacket}"/>
@@ -21,4 +24,5 @@ public record PacketEventArgs(PacketSource Source, string PacketString);
 /// <param name="Source">The source of the packet.</param>
 /// <param name="Packet">The deserialized packet.</param>
 /// <param name="PacketString">The packet string.</param>
-public record PacketEventArgs<TPacket>(PacketSource Source, TPacket Packet, string PacketString);
+[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Fix this.")]
+public record struct PacketEventArgs<TPacket>(PacketSource Source, TPacket Packet, string PacketString);
